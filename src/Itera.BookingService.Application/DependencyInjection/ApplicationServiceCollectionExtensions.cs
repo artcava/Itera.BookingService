31d101a1a966d129937376b0b51f9905aca45887
Marco Cavallo <cavallo.marco@gmail.com>
2026-06-29 16:26:49 +0200

    feat: register SecurityService and SecurityQueryService in DI

--- src/Itera.BookingService.Application/DependencyInjection/ApplicationServiceCollectionExtensions.cs
+++ src/Itera.BookingService.Application/DependencyInjection/ApplicationServiceCollectionExtensions.cs
@@ -1,6 +1,9 @@
 using FluentValidation;
 using Itera.BookingService.Application.Abstractions;
 using Itera.BookingService.Application.Branch;
+using Itera.BookingService.Application.Security.Dtos;
+using Itera.BookingService.Application.Security.Services;
+using Itera.BookingService.Application.Security.Validators;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Itera.BookingService.Application.DependencyInjection;
@@ -9,9 +12,15 @@ public static class ApplicationServiceCollectionExtensions
 {
     public static IServiceCollection AddBookingApplication(this IServiceCollection services)
     {
+        // Branch
         services.AddScoped<IValidator<Contracts.Legacy.Branch.WsGetAllFilialiRequest>, WsGetAllFilialiRequestValidator>();
         services.AddScoped<IValidator<Contracts.Legacy.Branch.WsGetFilialeInfoRequest>, WsGetFilialeInfoRequestValidator>();
         services.AddScoped<ILegacyBranchService, LegacyBranchService>();
+
+        // Security
+        services.AddScoped<IValidator<GetTokenRequest>, GetTokenRequestValidator>();
+        services.AddScoped<IValidator<ValidateTokenRequest>, ValidateTokenRequestValidator>();
+        services.AddScoped<ISecurityService, LegacySecurityService>();
 
         return services;
     }
