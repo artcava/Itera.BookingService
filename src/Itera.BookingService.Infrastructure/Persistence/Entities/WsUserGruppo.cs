c2021246837325d94e5676d3320f0aba6792f0ad
Marco Cavallo <cavallo.marco@gmail.com>
2026-06-30 11:47:49 +0200

    refactor(persistence): rimuove WsUserGruppo.cs dalla root Entities (spostato in Security/)

--- src/Itera.BookingService.Infrastructure/Persistence/Entities/WsUserGruppo.cs
+++
@@ -1,7 +0,0 @@
-namespace Itera.BookingService.Infrastructure.Persistence.Entities;
-
-public class WsUserGruppo
-{
-    public int WsUserGruppoID { get; set; }
-    public int WsUserID { get; set; }
-}
