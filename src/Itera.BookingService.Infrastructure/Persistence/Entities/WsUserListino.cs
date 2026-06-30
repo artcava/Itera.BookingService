d1f1525bbbcd744e91c5db8cb25fdbe94d42f30a
Marco Cavallo <cavallo.marco@gmail.com>
2026-06-30 11:47:54 +0200

    refactor(persistence): rimuove WsUserListino.cs dalla root Entities (spostato in Security/)

--- src/Itera.BookingService.Infrastructure/Persistence/Entities/WsUserListino.cs
+++
@@ -1,7 +0,0 @@
-namespace Itera.BookingService.Infrastructure.Persistence.Entities;
-
-public class WsUserListino
-{
-    public int WsUserListinoID { get; set; }
-    public int WsUserID { get; set; }
-}
