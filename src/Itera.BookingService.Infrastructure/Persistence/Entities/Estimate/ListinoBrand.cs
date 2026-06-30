fabb23334b78c8e932468c814eb57d56d2a8eeff
Marco Cavallo <cavallo.marco@gmail.com>
2026-06-30 15:08:29 +0200

    feat(estimate): add Km and ListinoBrand entity classes

---
+++ src/Itera.BookingService.Infrastructure/Persistence/Entities/Estimate/ListinoBrand.cs
@@ -0,0 +1,12 @@
+namespace Itera.BookingService.Infrastructure.Persistence.Entities;
+
+public class ListinoBrand
+{
+    public int ListinoBrandID { get; set; }
+    public short BrandID { get; set; }
+    public int ListinoID { get; set; }
+    public DateTime? DataInserimento { get; set; }
+    public DateTime? DataUltimaModifica { get; set; }
+
+    public Listino Listino { get; set; } = null!;
+}
