23e4c12819251f50f34856b9815c60ca8aea1bee
Marco Cavallo <cavallo.marco@gmail.com>
2026-07-01 14:35:30 +0200

    feat(estimate): add WsGetProvince contract DTO

---
+++ src/Itera.BookingService.Contracts/Legacy/Estimate/WsGetProvince.cs
@@ -0,0 +1,7 @@
+namespace Itera.BookingService.Contracts.Legacy.Estimate;
+
+public class WsGetProvince
+{
+    public string  CodiceProvincia      { get; set; } = default!;
+    public string? DescrizioneProvincia { get; set; }
+}
